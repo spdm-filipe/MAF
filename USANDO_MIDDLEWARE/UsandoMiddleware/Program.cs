a3cb4f6f5ede9c5fb7a1424c5b5f32b8f4b213d1
Filipe Augusto Santos Chaves <filipe.chaves@spdm-pais.org.br>
2026-07-01 13:24:05 -0300

    usando middleware

---
+++ USANDO_MIDDLEWARE/UsandoMiddleware/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+using OllamaSharp;
+
+IChatClient ollama = new OllamaApiClient(new Uri("http://localhost:11434"), "qwen2.5:latest");
+
+var agent = ollama
+    .AsAIAgent()
+    .AsBuilder()
+    .Use(inner => new LoggingAgentMiddleware(inner))
+    .Build();
+
+
+    var response = await agent.RunAsync("O que são records no C#? me responda em até 80 caracteres.");
+Console.WriteLine($"\nResposta Final: {response.Messages.Last().Text}");
+
+public class LoggingAgentMiddleware(AIAgent innerAgent) : DelegatingAIAgent(innerAgent)
+{
+    private readonly AIAgent _inner = innerAgent; // Guardamos a referência direta
+
+    protected override async Task<AgentResponse> RunCoreAsync(
+        IEnumerable<ChatMessage> messages,
+        AgentSession? session = null,
+        AgentRunOptions? options = null,
+        CancellationToken cancellationToken = default)
+    {
+        var lastMsg = messages.LastOrDefault()?.Text;
+        Console.WriteLine($"\n[LOG INTERCEPTOR] Pergunta: {lastMsg}");
+
+        var response = await _inner.RunAsync(messages, session, options, cancellationToken);
+
+        Console.WriteLine($"[LOG INTERCEPTOR] Resposta recebida do Gemma 4.");
+        return response;
+    }
+}
